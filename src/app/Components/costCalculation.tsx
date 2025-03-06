9527d3bc63cccfef06545e775ca814cd5321357c
bliz <uzomaebuka11@gmail.com>
2025-03-06 19:34:16 +0100

    Modified the function to calculate for shipment cost

--- src/app/Components/costCalculation.tsx
+++ src/app/Components/costCalculation.tsx
@@ -35,14 +35,13 @@ export default function CostCalculation(){
             const res = await fetch(url);
             const data = await res.json();
             const cords = data.features[0].geometry.coordinates
-            // console.log(cords)
             return cords;
         }catch(error){
             console.log(error);
         }
     }
-    // Haversine formular
     
+    // Haversine formular
     const  getDistance =(
         lat1: number,
         lon1: number,
@@ -73,26 +72,23 @@ export default function CostCalculation(){
         
         const from = await getCordinates(calculate.from);
         const to = await getCordinates(calculate.to);
-        
         const distance = getDistance(from[1], from[0], to[1], to[0]).toFixed(2);
         const distancCharge = Number(distance) *  0.05;
         const weightCharge = Number(calculate.weight) * 2;
         const heightInches = Number(calculate.height) / 2.54;
         const widthInches = Number(calculate.width) / 2.54;
         const depthInches = Number(calculate.depth) / 2.54;
-        const dimWeight = heightInches * widthInches * depthInches / 139;
+        const dimWeight = (heightInches * widthInches * depthInches) / 139;
         const dimWeightCharge = dimWeight * 2;
         const baseRate = 50;
 
-        console.log(distancCharge, weightCharge, heightInches, widthInches, depthInches, dimWeight, dimWeightCharge)
-
         if(dimWeightCharge > weightCharge){
             const shippingCost = baseRate + distancCharge + dimWeightCharge + Number(calculate.package);
-            const fixed = shippingCost.toFixed(0)
+            const fixed = shippingCost.toFixed(0);
             setCost(fixed);
         }else{
             const shippingCost = baseRate + distancCharge + weightCharge + Number(calculate.package);
-            const fixed = shippingCost.toFixed(0)
+            const fixed = shippingCost.toFixed(0);
             setCost(fixed);
             
         }
